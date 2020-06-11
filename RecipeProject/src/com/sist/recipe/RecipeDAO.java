365c92e6386225c18a08579a0bca8cc9c29ce057
jihyunkim-dollbi <dollbi5@gmail.com>
2020-06-11 14:33:46 +0900

    recipe mongoDB project

---
+++ RecipeProject/src/com/sist/recipe/RecipeDAO.java
@@ -0,0 +1,71 @@
+package com.sist.recipe;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.*;
+import java.util.List;
+import java.util.*;
+public class RecipeDAO {
+ 
+	private static SqlSessionFactory ssf;
+	static{
+		
+		
+		try{
+			
+			Reader reader=Resources.getResourceAsReader("Config.xml");
+			ssf=new SqlSessionFactoryBuilder().build(reader); //파싱끝!
+			
+			
+		}catch(Exception ex){
+			
+			System.out.println(ex.getMessage());
+		}
+		
+		
+	}
+	
+	
+	
+	//기능 설정!
+	public static void chefInsert(ChefVO vo){
+		
+		SqlSession session=ssf.openSession(true); //오토커밋
+		//Autocommit;
+		session.insert("chefInsert", vo);
+		session.close();
+	}
+	
+	
+	public static void recipeInsert(RecipeVO vo)
+	{
+		SqlSession session=ssf.openSession(true);
+		session.insert("recipeInsert", vo);
+		session.close();
+		
+		
+	}
+	
+	
+	public static void recipeDetailInsert(RecipeDetailVO vo)
+	{
+		SqlSession session=ssf.openSession(true);
+		session.insert("recipeDetailInsert", vo);
+		session.close();
+		
+		
+	}
+	
+	public static List<RecipeVO> recipeData(){
+		
+		SqlSession session=ssf.openSession();
+		List<RecipeVO> list=session.selectList("recipeData");
+		session.close();
+		
+		return list;
+		
+	}
+}
---
+++ RecipeProject/src/com/sist/recipe/RecipeMongoDAO.java
@@ -0,0 +1,140 @@
+package com.sist.recipe;
+import java.net.InetSocketAddress;
+import java.util.*;
+import com.mongodb.*;
+public class RecipeMongoDAO {
+    private MongoClient mc;
+    private DB db;
+    private DBCollection dbc;
+    
+    @SuppressWarnings("deprecation")
+	public RecipeMongoDAO()
+    {
+    	try
+    	{
+    		mc=new MongoClient(new ServerAddress("localhost",27017));
+    		//mc=new MongoClient(new ServerAddress(new InetSocketAddress("localhost",27017)));
+    		db=mc.getDB("mydb");
+    		
+    	}catch(Exception ex){}
+    }
+    /*
+     *  private int no;
+	    private String title;
+	    private String poster;
+	    private String chef;
+	    private String link;
+     */
+    public void recipeInsert(RecipeVO vo)
+    {
+    	try
+    	{
+    		int max=0;
+    		DBCursor cursor=dbc.find();
+    		while(cursor.hasNext())
+    		{
+    			 BasicDBObject data=(BasicDBObject)cursor.next();
+    			 int no=data.getInt("no");
+    			 if(max<no)
+    				 max=no;
+    		}
+    		cursor.close();
+    		BasicDBObject obj=new BasicDBObject();
+    		obj.put("no", max+1);
+    		obj.put("title", vo.getTitle());
+    		obj.put("poster", vo.getPoster());
+    		obj.put("chef", vo.getChef());
+    		obj.put("link", vo.getLink());
+    		
+    		dbc.insert(obj);
+    	}catch(Exception ex)
+    	{
+    		System.out.println(ex.getMessage());
+    	}
+    }
+    /*
+     * private String poster;
+    private String chef;
+    private String mem_cont1;
+    private String mem_cont3;
+    private String mem_cont7;
+    private String mem_cont2;
+     */
+    public List<RecipeVO> recipeListData()
+    {
+    	List<RecipeVO> list=new ArrayList<RecipeVO>();
+    	try
+    	{
+    		dbc=db.getCollection("recipe");
+    		DBCursor cursor=dbc.find();
+    		int k=1;
+    		while(cursor.hasNext())
+    		{
+    			BasicDBObject obj=(BasicDBObject)cursor.next();
+    			RecipeVO vo=new RecipeVO();
+    			vo.setNo(obj.getInt("no"));
+    			vo.setLink(obj.getString("link"));
+    			list.add(vo);
+    			System.out.println("k="+k);
+    			k++;
+    		}
+    		cursor.close();
+    	}catch(Exception ex){}
+    	return list;
+    }
+    public void chefInsert(ChefVO vo)
+    {
+    	try
+    	{
+    		BasicDBObject obj=new BasicDBObject();
+    		obj.put("poster", vo.getPoster());
+    		obj.put("chef", vo.getChef());
+    		obj.put("mem_cont1", vo.getMem_cont1());
+    		obj.put("mem_cont3", vo.getMem_cont3());
+    		obj.put("mem_cont7", vo.getMem_cont7());
+    		obj.put("mem_cont2", vo.getMem_cont2());
+    		
+    		dbc.insert(obj);
+    	}catch(Exception ex)
+    	{
+    		System.out.println(ex.getMessage());
+    	}
+    }
+    /*
+     *  private int no;
+	    private String poster;
+	    private String title;
+	    private String chef;
+	    private String chef_poster;
+	    private String chef_profile;
+	    private String info1,info2,info3;
+	    private String content;
+	    private String foodmake;
+     */
+    public void recipeDetailInsert(RecipeDetailVO vo)
+    {
+    	try
+    	{
+    		dbc=db.getCollection("recipe_detail");
+    	    BasicDBObject obj=new BasicDBObject();
+    	    obj.put("no", vo.getNo());
+    	    obj.put("poster", vo.getPoster());
+    	    obj.put("title", vo.getTitle());
+    	    obj.put("chef", vo.getChef());
+    	    obj.put("chef_poster", vo.getChef_poster());
+    	    obj.put("chef_profile", vo.getChef_profile());
+    	    obj.put("info1", vo.getInfo1());
+    	    obj.put("info2", vo.getInfo2());
+    	    obj.put("info3", vo.getInfo3());
+    	    obj.put("content", vo.getContent());
+    	    obj.put("foodmake", vo.getFoodmake());
+    	    
+    	    dbc.insert(obj);
+    	}catch(Exception ex){}
+    }
+}
+
+
+
+
+
